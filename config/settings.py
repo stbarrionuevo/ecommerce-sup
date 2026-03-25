d1a47e4e4201374e63edfbec837119b152ec0acc
stbarrionuevo <santiagobarrionuevo27@gmail.com>
2026-03-25 17:41:07 -0300

    Agrego STATICFILES para CSS

--- config/settings.py
+++ config/settings.py
@@ -149,6 +149,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
